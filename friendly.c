6a478bdc672e9be3ad98f94959f4659880d0d76e
Tom Klinovsky <tom@kiwinet.co.il>
2018-09-26 17:53:00 +0300

    fix for the edge case that a devidor is exactly the root of the candidate

--- friendly.c
+++ friendly.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned long calculateFriend(unsigned long candidate) {
+unsigned long sumDevidors(unsigned long candidate) {
   unsigned long i, sum = 1;
-  for (i = 2; i*i <= candidate; i++) {
+  for (i = 2; i*i < candidate; i++) {
     if (candidate % i == 0) {
       sum = sum + i + (candidate / i);
     }
+  }
+  if (i == candidate) {
+	  return sum + i;
   }
   return sum;
 }
@@ -16,10 +19,10 @@ int main(unsigned long argc, char *argv[]) {
   char *endptr;
   maxSearch = strtoul(argv[1], &endptr, 10);
   for (candidate = 1; candidate < maxSearch; candidate++) {
-    mate = calculateFriend(candidate);
+    mate = sumDevidors(candidate);
     if (mate <= candidate)
       continue;
-    if(calculateFriend(mate) == candidate) {
+    if(sumDevidors(mate) == candidate) {
       printf("%lu %lu\n", candidate, mate);
     }
   }
